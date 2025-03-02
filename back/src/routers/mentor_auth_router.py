7ad52fac36365649b964d36f0bc0343237758bde
depost <alex.vedeniapin123@gmail.com>
2025-03-03 02:02:33 +0300

    Добавление информации о свободных днях ментора в роутер аутентификации
    - Расширение ответа роутера аутентификации полем free_days
    - Включение информации о свободных днях в данные текущего пользователя

--- back/src/routers/mentor_auth_router.py
+++ back/src/routers/mentor_auth_router.py
@@ -71,6 +71,7 @@ async def get_current_user_info(
         "login": current_user.login,
         "is_active": current_user.is_active,
         "avatar_url": avatar_url,
+        "free_days": current_user.free_days,
         "created_at": current_user.created_at,
         "updated_at": current_user.updated_at
     }
